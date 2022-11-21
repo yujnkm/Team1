3ee848b5aead2fe0e8e92615a1ec7b6d48d308cb
kathywu1201 <kathyyebing@gmail.com>
2022-11-21 15:09:52 -0800

    Upgrade package import and update Maincontroller

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -9,140 +9,138 @@ using UnityEngine.Video;
  */
 public class PlayVideo : MonoBehaviour
 {
-    public float timeToFade;
-    public float withinDist;
-    public GameObject spiralSystem;
+    public float fadeTime;
+    public GameObject activator;
 
     private VideoPlayer videoPlayer;
-    private bool isPlayed;
     private bool fadeIn;
-    private float timeIn;
     private bool fadeOut;
-    private float timeOut;
-    private Renderer renderer;
-    private GameObject videoControl;
-    private GameObject[] positionControls;
+    private Renderer _renderer;
+    private VideoActivator videoActivator;
+    private Transform[] specialEffect;
 
+    void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
     void OnEnable()
     {
-        videoPlayer = GetComponent<VideoPlayer>();
-        renderer = GetComponent<Renderer>();
-        videoControl = GameObject.FindGameObjectWithTag("PositionController");
-        findPlayPoints();
-        Color color = renderer.material.color;
-        color.a = 0f;
-        renderer.material.color = color;
-        isPlayed = false;
         fadeIn = false;
-        timeIn = 0f;
         fadeOut = false;
-        timeOut = 0f;
-        /*
-         * spiral is deactivated to not confuse the user
-         * will be activated once the user finishes watching the main dancer footage (located at the end of the hall)
-         */
-        spiralSystem.SetActive(false);
-    }
+        Color color = _renderer.material.color;
+        color.a = 0f;
+        _renderer.material.color = color;
 
+        TutorialController.Instance.incrementTotalDancers();
+    }
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoActivator = activator.GetComponent<VideoActivator>();
+        specialEffect = this.gameObject.GetComponentsInChildren<Transform>();
+    }
     void Update()
     {
         /*
-         * Not used in study
-         * Initially designed to allow user to manually play the dancer video,
-         * but users did not have remote to do so
+         * users can use remote to replay video if within activator box
+         * checks if 1) user clicked button 2) video played once already 3) user is within box
          */
-        if (Input.GetKeyDown(KeyCode.V) && !videoPlayer.isPlaying && isPlayed)
+        if (Input.GetKeyDown(KeyCode.V) && videoActivator.GetActivationStatus() && videoActivator.IsWithin())
         {
-            Debug.Log("clicked");
             fadeIn = true;
             StartCoroutine(PlayDancerVideo());
         }
 
-        //fades in and out the dancer video when appropriate
+        //plays video when "activator" is activated
+        if (CheckPlay())
+        {
+            fadeIn = true;
+            TutorialController.Instance.incrementViewedDancers();
+            StartCoroutine(PlayDancerVideo());
+        }
+
+        /*
+         * fade transitions
+         * currently won't show up due to cutout material
+         * using cutout instead of fade material for optimization
+         */
         if (fadeIn)
         {
-            timeIn += Time.deltaTime;
-            Color color = renderer.material.color;
-            color.a = color.a + 1f / timeToFade * Time.deltaTime;
-            //color.a = color.a + 1f;
+            Color color = _renderer.material.color;
+            color.a = color.a + 1f / fadeTime * Time.deltaTime;
             if (color.a > 1)
             {
                 color.a = 1;
                 fadeIn = false;
             }
-            renderer.material.color = color;
+            _renderer.material.color = color;
         }
         if (fadeOut)
         {
-            timeIn += Time.deltaTime;
-            Color color = renderer.material.color;
-            color.a = color.a - 1f / timeToFade * Time.deltaTime;
-            //color.a = color.a - 1f;
+            Color color = _renderer.material.color;
+            color.a = color.a - 1f / fadeTime * Time.deltaTime;
             if (color.a < 0)
             {
                 color.a = 0;
                 fadeOut = false;
             }
-            renderer.material.color = color;
-        }
-
-        float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
-
-        /*
-         * plays video when user is a certain distance away from the dancers
-         */
-        if (!isPlayed && checkPlay())
-        {
-            isPlayed = true;
-            fadeIn = true;
-            StartCoroutine(PlayDancerVideo());
+            _renderer.material.color = color;
         }
     }
-    private void findPlayPoints()
+    public bool CheckPlay()
     {
-        positionControls = new GameObject[videoControl.transform.childCount];
-        for (int i = 0; i < positionControls.Length; i++)
-        {
-            positionControls[i] = videoControl.transform.GetChild(i).gameObject;
-        }
-    }
-    public bool checkPlay()
-    {
-        foreach (GameObject position in positionControls)
-        {
-            PlayVideoController videoController = position.GetComponent<PlayVideoController>();
-            if (videoController.dancer == this.gameObject)
-            {
-                return videoController.getStatus();
-            }
-        }
-        return false;
-    }
-    public bool getIsPlayed()
-    {
-        return isPlayed;
-    }
-    public GameObject getParent()
-    {
-        return this.transform.parent.gameObject;
+        return videoActivator.GetReadyStatus();
     }
     /*
-     * Fades in the dancer video and then plays it
-     * Once the video is done playing, the spiral system will activate,
-     * and it has a collider that will allow users to jump to an empty scene,
-     * where they will fill out the survey before moving on to the next scene
+     * fade out arrow
+     * fade in dancer video and plays
+     * plays special effects attached to dancer video
      */
     IEnumerator PlayDancerVideo()
     {
+        videoActivator.Completed();
+        ArrowController.Instance.ArrowFadeOut();
         videoPlayer.Play();
-        yield return new WaitForSeconds((float) videoPlayer.length - timeToFade);
-        fadeOut = true;
-        yield return new WaitForSeconds(timeToFade);
-        if (!spiralSystem.activeSelf && this.gameObject.tag == "MainDance")
+
+        /*
+         * activates special effects
+         * currently accounts of videos, particle systems, and audio
+         */
+        foreach (Transform effect in specialEffect)
         {
-            spiralSystem.SetActive(true);
-            spiralSystem.GetComponent<ParticleSystem>().Play();
-            GameObject.FindObjectOfType<IndicatorSystemController>().isDone = true;
+            if (effect.gameObject.GetComponent<VideoPlayer>() != null)
+            {
+                effect.gameObject.GetComponent<VideoPlayer>().Play();
+            }
+            if (effect.gameObject.GetComponent<ParticleSystem>() != null)
+            {
+                effect.gameObject.GetComponent<ParticleSystem>().Play();
+            }
+            if (effect.gameObject.GetComponent<AudioSource>() != null)
+            {
+                effect.gameObject.GetComponent <AudioSource>().Play();
+            }
         }
+        yield return new WaitForSeconds((float) videoPlayer.length - fadeTime);
+        fadeOut = true;
+        yield return new WaitForSeconds(fadeTime);
+
+        //stops special effects
+        foreach (Transform effect in specialEffect)
+        {
+            if (effect.gameObject.GetComponent<VideoPlayer>() != null)
+            {
+                effect.gameObject.GetComponent<VideoPlayer>().Stop();
+            }
+            if (effect.gameObject.GetComponent<ParticleSystem>() != null)
+            {
+                effect.gameObject.GetComponent<ParticleSystem>().Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+            if (effect.gameObject.GetComponent<AudioSource>() != null)
+            {
+                effect.gameObject.GetComponent<AudioSource>().Stop();
+            }
+        }
+        ArrowController.Instance.ArrowFadeIn();
     }
 }
